85fbfc1620e38e08141a5341c3a5e80785a846da
iAmAli <ali.naderi72@gmail.com>
2022-02-14 12:18:42 +0330

    created navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import GlobalStyle from "./styles/Global";
 import { ThemeProvider } from "styled-components";
 // STYLES
 import { Container } from "./styles/Container";
+// COMPONENTS
+import Navbar from "./components/Navbar";
 
 const theme = {
   'Orange': 'hsl(26, 100%, 55%)',
   'Pale orange': 'hsl(25, 100%, 94%)',
-  'Very vark blue': 'hsl(220, 13%, 13%)',
+  'Very dark blue': 'hsl(220, 13%, 13%)',
   'Dark grayish blue': 'hsl(219, 9%, 45%)',
   'Grayish blue': 'hsl(220, 14%, 75%)',
   'Light grayish blue': 'hsl(223, 64%, 98%)',
@@ -21,7 +23,7 @@ function App() {
     <ThemeProvider theme={ theme }>
       <GlobalStyle />
       <Container>
-
+        <Navbar />
       </Container>
     </ThemeProvider>
   );
